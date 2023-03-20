1882de1c513d6b7a0389b32de09c724af68a1f91
baiyuqi <1353813001@qq.com>
2023-03-20 12:05:07 +0800

    ArtistNFT

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const ArtistNFT = await ethers.getContractFactory("ArtistNFT");
+  const nft = await ArtistNFT.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await nft.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `ArtistNFT   deployed to ${nft.address}`
   );
 }
 
